0178b54f7cae8b4cc25fc1465d4cd4ce2ddeff53
lucaso98 <lucas.fransson98@gmail.com>
2022-03-03 22:53:12 +0100

    New hero-image + shadow

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 <main class="absolute grid right-0 w-full min-h-screen bg-black duration-700 z-10">
     <video class="absolute top-0 left-0 w-full h-full object-cover opacity-80" src="./static/background-video.mp4" muted loop autoplay />
     <div
-        class="relative z-20 m-auto shadow-md shadow-neutral-600 flex p-10 h-4/6 w-4/6 bg-transparent backdrop-blur-md rounded-3xl"
+       id="glass" class="relative z-20 m-auto flex p-14 h-4/6 w-4/6 bg-transparent backdrop-blur-md rounded-3xl"
     >
         <div class="self-center w-2/4">
             <h1 class="text-white text-6xl font-bold mb-2">Lucas Fransson</h1>
@@ -29,4 +29,10 @@
 		background-repeat: no-repeat;
 		background-size: cover;
 	}
+
+    #glass{
+        -webkit-box-shadow: 0px 10px 25px 5px rgba(0,0,0,0.5); 
+        box-shadow: 0px 10px 25px 10px rgba(0,0,0,0.7);
+        border: 1px solid rgba(255, 255, 255, 0.1);
+    }
 </style>
